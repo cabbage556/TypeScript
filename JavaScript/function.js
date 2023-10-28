3c73be9cc1b708e8f56d16948158b419b3ffec76
cabbage556 <liberty556@gmail.com>
2023-10-28 22:07:12 +0900

    JavaScript 함수

---
+++ JavaScript/function.js
@@ -0,0 +1,27 @@
+/**
+ * arguments 객체
+ *
+ * 함수에 입력한 아규먼트들을 값으로 갖는 객체
+ */
+const multiply = function (x, y, z) {
+  console.log(arguments);
+};
+multiply(4, 5, 6); // [Arguments] { '0': 4, '1': 5, '2': 6 }
+
+const multiplyAll = function (...args) {
+  return Object.values(arguments).reduce((pre, cur) => pre * cur, 1);
+};
+console.log(multiplyAll(10, 20, 30)); // 6000
+
+/**
+ * 즉시 실행 함수
+ */
+(function (x, y) {
+  console.log(x * y);
+})(5, 10); // 50
+
+/**
+ * 함수의 타입은 객체
+ */
+console.log(typeof multiply); // function
+console.log(multiply instanceof Object); // true: 함수는 객체다
